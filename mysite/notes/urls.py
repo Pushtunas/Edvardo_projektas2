079b0e4a72f1c58e5d3fef025de4e95dd4f4762c
Edvardas <edvardas.giedrys@codeacademy.lt>
2022-12-03 23:02:11 +0200

    Sukurti views, pridėti path ir perkeltas base

--- mysite/notes/urls.py
+++ mysite/notes/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import index
+from .import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    path('main/', views.main, name='main'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('update/', views.profile, name='update'),
 ]
--- mysite/notes/views.py
+++ mysite/notes/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('veikia')
+    return render(request, 'notes/index.html')
+
+def main(request):
+    return render(request, 'notes/main.html')
+
+def register(request):
+    return render(request, 'registration/register.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+def profile(request):
+    return render(request, 'registration/profile.html')
+
+def logout(request):
+    return render(request, 'registration/logout.html')
+
+def update(request):
+    return render(request, 'notes/update.html')
